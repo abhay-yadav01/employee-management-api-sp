63325ccbc1c5101cab16833756544a1973a1c3a4
Abhay Yadav <abhayyadav19110@gmail.com>
2026-02-13 18:00:14 +0530

    configuring DataBaseContext

---
+++ EmployeeManagementAPI/EmployeeManagementAPI/Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManagementAPI.Models.Entities;
+
+namespace EmployeeManagementAPI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
