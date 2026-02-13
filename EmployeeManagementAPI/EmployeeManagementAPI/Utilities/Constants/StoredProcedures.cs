faad193e411a51464c355bdc58d23a066bdb55af
Abhay Yadav <abhayyadav19110@gmail.com>
2026-02-13 18:14:04 +0530

    Update stored procedure constants

--- EmployeeManagementAPI/EmployeeManagementAPI/Utilities/Constants/StoredProcedures.cs
+++ EmployeeManagementAPI/EmployeeManagementAPI/Utilities/Constants/StoredProcedures.cs
@@ -1,6 +1,11 @@
 ﻿namespace EmployeeManagementAPI.Utilities.Constants
 {
-    public class StoredProcedures
+    public static class StoredProcedures
     {
+        public const string CreateEmployee = "SP_CreateEmployee";
+        public const string GetEmployeeById = "SP_GetEmployeeById";
+        public const string GetAllEmployees = "SP_GetAllEmployees";
+        public const string UpdateEmployee = "SP_UpdateEmployee";
+        public const string DeleteEmployee = "SP_DeleteEmployee";
     }
 }
